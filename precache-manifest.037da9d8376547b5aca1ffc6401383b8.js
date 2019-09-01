af0d4aa5f47930083f94bae15931c3a2949dd2f8
hollandjb <hollandjb@vcu.edu>
2019-08-31 20:50:07 -0400

    Updates

---
+++ precache-manifest.037da9d8376547b5aca1ffc6401383b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57642fb18b62fc9f2688e3ae80ce46e5",
+    "url": "/FirstSimpleReactApp/index.html"
+  },
+  {
+    "revision": "498bffa7a74ce72c6ae9",
+    "url": "/FirstSimpleReactApp/static/css/main.a7673a41.chunk.css"
+  },
+  {
+    "revision": "698270fb909c843e2982",
+    "url": "/FirstSimpleReactApp/static/js/2.3df7f6a2.chunk.js"
+  },
+  {
+    "revision": "498bffa7a74ce72c6ae9",
+    "url": "/FirstSimpleReactApp/static/js/main.bb69afe3.chunk.js"
+  },
+  {
+    "revision": "c302201c8d4e2f45df1a",
+    "url": "/FirstSimpleReactApp/static/js/runtime~main.1e56f0f9.js"
+  }
+]);
